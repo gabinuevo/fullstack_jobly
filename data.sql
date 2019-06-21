98746ee29eb4c34e27489d65e0196b6fcf7cefb4
Gabriela <balicas.gb@gmail.com>
2019-06-20 18:16:41 -0700

    update seed file to reflect new users table

--- data.sql
+++ data.sql
@@ -1,3 +1,11 @@
+DROP DATABASE IF EXISTS  jobly;
+
+DROP TABLE IF EXISTS  companies;
+DROP TABLE IF EXISTS  jobs;
+DROP TABLE IF EXISTS  users;
+
+CREATE DATABASE jobly;
+
 CREATE TABLE companies (
     handle text PRIMARY KEY,
     name text NOT NULL UNIQUE,
@@ -14,6 +22,17 @@ CREATE TABLE jobs (
     company_handle text NOT NULL REFERENCES companies ON DELETE CASCADE,
     date_posted timestamp without time zone DEFAULT NOW() NOT NULL
     CONSTRAINT equity CHECK ((equity<=1))
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username text NOT NULL,
+    password text NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    email text NOT NULL UNIQUE,
+    photo_url text,
+    is_admin BOOLEAN DEFAULT FALSE
 );
 
 -- CREATE TABLE messages (
