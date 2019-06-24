a3e2ba370eb3356d738317d1f84b0127f83fc322
Gabriela <balicas.gb@gmail.com>
2019-06-24 12:49:08 -0700

    update styling for individual company pages

--- FRONTEND/src/Company.js
+++ FRONTEND/src/Company.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import JoblyApi from './JoblyAPI';
 import JobCard from './JobCard';
 
+import './Company.css';
+
 /**
  * Recieves ONE company data from API request
  * Renders ONE company details
@@ -19,13 +21,13 @@ class Company extends Component {
   // gets company data and adds it to state. changes
   // loading to false
   async componentDidMount() {
-    try{
+    try {
       let data = await JoblyApi.getCompany(this.props.handle);
       this.setState({
         companyData: data,
         loading: false
       });
-    } catch(err) {
+    } catch (err) {
       this.setState({
         error: err
       });
@@ -34,42 +36,45 @@ class Company extends Component {
 
   render() {
     // set of job ids of jobs user has applied to
-    const appliedJobs = new Set(this.props.currJobs.map( job => job.id ));
+    const appliedJobs = new Set(this.props.currJobs.map(job => job.id));
     const company = this.state.companyData;
     let jobs;
-  
+
     if (!this.state.loading) {
-      jobs = this.state.companyData.jobs.map( job => 
-        <JobCard 
-          title={job.title} 
-          salary={job.salary} 
+      jobs = this.state.companyData.jobs.map(job =>
+        <JobCard
+          title={job.title}
+          salary={job.salary}
           equity={job.equity}
-          key={ job.id }
-          applied={ appliedJobs.has(job.id) }
-          triggerApply={ () => this.props.triggerApply(job.id) }
+          key={job.id}
+          applied={appliedJobs.has(job.id)}
+          triggerApply={() => this.props.triggerApply(job.id)}
         />
       )
     }
 
     return (
       <div className="Company">
-        { this.state.error && this.state.loading
+        {this.state.error && this.state.loading
           ? <p>Loading...</p>
           : <div>
-              <p className="Company-name">
-                { company.name }
-              </p>
+            <h2 className="Company-name">
+              {company.name}
+            </h2>
 
-              <p className="Company-desc">
-                { company.description }
-              </p>
-
+            <p className="Company-desc">
+              --{company.description}
+            </p>
+            <br></br>
+            <div className="Company-Openings">
+              <h4 className="Company-Job-Openings-Title"> Open positions: </h4>
               <div className="Company-jobs">
                 {jobs}
               </div>
+            </div>
           </div>
-        } 
-        { this.state.error && <p>Something went wrong</p>}
+        }
+        {this.state.error && <p>Something went wrong</p>}
       </div>
     );
   }
