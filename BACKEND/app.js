78e55c9b2b71aa954aa107dc73af4a769b6c04ef
Gabriela <balicas.gb@gmail.com>
2019-06-26 17:40:04 -0700

    add config to disable cors in heroku

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -6,6 +6,27 @@ const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(allowCrossDomain);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
 app.use(express.json());
 app.use(cors());
 
