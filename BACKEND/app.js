70d66077246171d06e5c3bf85a6ca0e789e735f7
Gabriela <balicas.gb@gmail.com>
2019-06-26 18:03:35 -0700

    change CORS configuration order to try prevent the OPTIONS from erroring out

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 var allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
   // intercept OPTIONS method
@@ -20,9 +20,9 @@ var allowCrossDomain = function (req, res, next) {
   }
 };
 app.configure(function () {
+  app.use(allowCrossDomain);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(allowCrossDomain);
   app.use(express.static(path.join(application_root, "public")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
